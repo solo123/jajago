a1cf217295d8b3a1efa5f3b16d9279b6a62c007e
Jimmy Liang <jimmy@coolpur.com>
2011-01-04 10:54:04 +0800

    test

--- SA/Biz/ResourceManager.cs
+++ SA/Biz/ResourceManager.cs
@@ -20,5 +20,38 @@ namespace com.jajago.SA.Biz
             return 0;
         }
 
+        public static void ListFiles(FileSystemInfo info, string Ext, TextBox obj)
+        {
+            if (!info.Exists) return;
+
+            DirectoryInfo dir = info as DirectoryInfo;
+            //不是目录 
+            if (dir == null) return;
+            try
+            {
+
+                FileSystemInfo[] files = dir.GetFileSystemInfos();
+                for (int i = 0; i < files.Length; i++)
+                {
+                    FileInfo file = files[i] as FileInfo;
+                    //是文件
+                    if (file != null && file.Extension.ToUpper() == "." + Ext.ToUpper())
+                    {
+                        obj.Text = obj.Text + file.FullName + "\r\n";
+                        obj.Refresh();
+                    }
+                    //对于子目录，进行递归调用 
+                    else
+                        ListFiles(files[i], Ext, obj);
+
+                }
+            }
+            catch (UnauthorizedAccessException ex)
+            {
+                obj.Text = obj.Text + ex.Message;
+            }
+
+        }
+
     }
 }
