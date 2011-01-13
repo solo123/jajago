b97b76f35fd2197a3de0c6b51d6cea16316798d6
Jimmy Liang <jimmy@coolpur.com>
2011-01-13 15:42:40 +0800

    mobile

--- DI/export_mobiles.rb
+++ DI/export_mobiles.rb
@@ -14,17 +14,33 @@ end
 class EcsGood < JajagoDB
   set_primary_key "goods_id"
 end 
+class EcsAttribute < JajagoDB
+  set_table_name "ecs_attribute"
+  set_primary_key "attr_id"
+end
+class EcsGoodsAttr < JajagoDB
+  set_table_name "ecs_goods_attr"
+  set_primary_key "goods_attr_id"
+end
 
 def gen_mobile(mobile)
-  s = "INSERT INTO Mobiles (id, title, standard, description, price_shop, price_market) VALUES (#{mobile.goods_id}, '#{mobile.goods_name}', 'standard', '#{sql_escape(mobile.goods_desc)}', '#{mobile.shop_price}', '#{mobile.shopout_price}')\ngo\n" 
+  "INSERT INTO Mobiles (id, title, description, price_shop, price_market) VALUES (#{mobile.goods_id}, '#{mobile.goods_name}', '#{sql_escape(mobile.goods_desc)}', '#{mobile.shop_price}', '#{mobile.shopout_price}')\ngo\n" 
+end
+def gen_attribute(attribute)
+  "INSERT INTO MobileAttributes (id,title) VALUES (#{attribute.attr_id}, '#{sql_escape attribute.attr_name}')\ngo\n"
+end
+def gen_mobile_attr(attr)
+  "INSERT INTO MobileInAttributes (id,mobile_id,attribute_id,title) VALUES (#{attr.id}, #{attr.goods_id}, #{attr.attr_id}, '#{sql_escape attr.attr_value}')\ngo\n"
 end
 def sql_escape(str)
   str.gsub(/['\n\r]/, "'" => "''", "\n" => '\n', "\r" => '')
 end
 
 File.open('out.txt', 'w') do|f| 
-  EcsGood.where('goods_id<10').order('goods_id').each do |m|
-    f.puts gen_mobile(m)
-    
+  # attributes
+  EcsAttribute.all.each { |attr| f.puts gen_attribute(attr) }
+  EcsGood.where('goods_id<10').order('goods_id').each do |m| 
+	f.puts gen_mobile(m)
+	EcsGoodsAttr.where('goods_id=?', m.id).order('attr_id').each { |attr| f.puts gen_mobile_attr(attr) }
   end
 end
