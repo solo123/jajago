b34afcb40e44e0181200afc6b5f4b666366e49e7
jy <yjfccjb@sina.com>
2011-01-25 15:59:46 +0800

    a

--- Ctls/CtlImageList.cs
+++ Ctls/CtlImageList.cs
@@ -34,42 +34,42 @@ namespace com.jajago.SA.Ctls
         private void listMusics_CellClick(object sender, DataGridViewCellEventArgs e)
         {
             int idx = e.RowIndex;
-            if (idx < 0) return;
-            CtlMusicList c = new CtlMusicList();
-            if (rm == null) rm = ResourceManager.Instance;
-            DataGridViewRow crow = listData.Rows[idx];
-            Resource res = (Resource)crow.DataBoundItem;
-            c.a(idx, res, crow);
             //if (idx < 0) return;
-
+            //CtlMusicList c = new CtlMusicList();
             //if (rm == null) rm = ResourceManager.Instance;
             //DataGridViewRow crow = listData.Rows[idx];
             //Resource res = (Resource)crow.DataBoundItem;
-            //if (_selected_items.IndexOf(idx) < 0)
-            //{
-            //    _selected_items.Add(idx);
-            //    foreach (DataGridViewCell cell in crow.Cells)
-            //    {
-            //        cell.Style.BackColor = SystemColors.Highlight;
-            //        cell.Style.ForeColor = SystemColors.HighlightText;
-            //    }
-            //    crow.HeaderCell.Value = "√";
-            //    SelectedCount++;
-            //    if (res!=null && res.size!=null) SelectedSize += res.size.Value;
-            //}
-            //else
-            //{
-            //    _selected_items.Remove(idx);
-            //    foreach (DataGridViewCell cell in listData.Rows[e.RowIndex].Cells)
-            //    {
-            //        cell.Style.BackColor = SystemColors.Control;
-            //        cell.Style.ForeColor = SystemColors.WindowText;
-            //    }
-            //    crow.HeaderCell.Value = "";
-            //    SelectedCount--;
-            //    if (res != null && res.size != null) SelectedSize -= res.size.Value;
-            //}
-            //base.OnSelectChanged(this, null);
+            //c.a(idx, res, crow);
+            if (idx < 0) return;
+
+            if (rm == null) rm = ResourceManager.Instance;
+            DataGridViewRow crow = listData.Rows[idx];
+            Resource res = (Resource)crow.DataBoundItem;
+            if (_selected_items.IndexOf(idx) < 0)
+            {
+                _selected_items.Add(idx);
+                foreach (DataGridViewCell cell in crow.Cells)
+                {
+                    cell.Style.BackColor = SystemColors.Highlight;
+                    cell.Style.ForeColor = SystemColors.HighlightText;
+                }
+                crow.HeaderCell.Value = "√";
+                SelectedCount++;
+                if (res != null && res.size != null) SelectedSize += res.size.Value;
+            }
+            else
+            {
+                _selected_items.Remove(idx);
+                foreach (DataGridViewCell cell in listData.Rows[e.RowIndex].Cells)
+                {
+                    cell.Style.BackColor = SystemColors.Control;
+                    cell.Style.ForeColor = SystemColors.WindowText;
+                }
+                crow.HeaderCell.Value = "";
+                SelectedCount--;
+                if (res != null && res.size != null) SelectedSize -= res.size.Value;
+            }
+            base.OnSelectChanged(this, null);
         }
     }
 }
